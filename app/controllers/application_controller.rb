415858d981facecdf3da7be64d3d80980869bfe5
berubenic <berubenic@gmail.com>
2021-08-19 09:38:24 -0400

    routes when signed_in and not signed_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || user_root_url
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname last_name birthdate])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name birthdate])
   end
 end
---
+++ app/controllers/feed_controller.rb
@@ -0,0 +1,3 @@
+class FeedController < ApplicationController
+  def index; end
+end
