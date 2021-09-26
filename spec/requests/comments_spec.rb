6bbc0a893f0c3f066b791f2a2d5895c0bdc1d678
Nicholas <berubenic@gmail.com>
2021-09-26 18:37:26 -0400

    create comment spec when valid attributes

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
   fixtures :users, :posts, :comments
+
+  describe '#create' do
+    it 'creates comment with valid attributes' do
+      sign_in users(:user_one)
+      post = posts(:first_post)
+      comment_params = {
+        comment: {
+          body: 'Comment!'
+        },
+        post_id: post.id.to_s
+      }
+      post "/posts/#{post.id}/comments", params: comment_params
+      expect(flash[:success]).to eq('Comment created!')
+      expect(response).to have_http_status(302)
+    end
+
+    it 'does not create comment with non-valid attributes' do
+    end
+  end
 end
