f43fc9627d2a0aad8c2f1a1ee5052d379e228387
Nicholas <berubenic@gmail.com>
2021-10-16 11:22:09 -0400

    user submits invalid sign up form feature spec

--- spec/features/user_submits_sign_up_form_spec.rb
+++ spec/features/user_submits_sign_up_form_spec.rb
@@ -1,19 +1,48 @@
 require 'rails_helper'
-RSpec.feature 'User submits a form' do
-  scenario 'they see the page with the form' do
-    user_email = 'joe_picket@email.com'
-    user_first_name = 'Joe'
-    user_last_name = 'Picket'
-    user_password = 'somehardcorepassword1234!'
-    visit root_path
-    click_on 'Sign up', match: :first
-    fill_in 'user_email', with: user_email
-    fill_in 'user_first_name', with: user_first_name
-    fill_in 'user_last_name', with: user_last_name
-    fill_in 'user_password', with: user_password
-    fill_in 'user_password_confirmation', with: user_password
 
-    find('input[type="submit"]').click
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
+RSpec.feature 'User submits a form' do
+  context 'the form is valid' do
+    scenario 'they see the page with the form' do
+      user_email = 'joe_picket@email.com'
+      user_first_name = 'Joe'
+      user_last_name = 'Picket'
+      user_password = 'somehardcorepassword1234!'
+
+      visit root_path
+
+      click_on 'Sign up', match: :first
+
+      fill_in 'user_email', with: user_email
+      fill_in 'user_first_name', with: user_first_name
+      fill_in 'user_last_name', with: user_last_name
+      fill_in 'user_password', with: user_password
+      fill_in 'user_password_confirmation', with: user_password
+
+      find('input[type="submit"]').click
+
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+  end
+
+  context 'the form is invalid' do
+    scenario 'they see the page with the form' do
+      user_email = 'joe_picket@email.com'
+      user_first_name = 'Joe'
+      user_last_name = 'Picket'
+      user_password = 'somehardcorepassword1234!'
+
+      visit root_path
+
+      click_on 'Sign up', match: :first
+
+      fill_in 'user_email', with: user_email
+      fill_in 'user_first_name', with: user_first_name
+      fill_in 'user_last_name', with: user_last_name
+      fill_in 'user_password', with: user_password
+
+      find('input[type="submit"]').click
+
+      expect(page).to have_content "Password confirmation doesn't match Password"
+    end
   end
 end
