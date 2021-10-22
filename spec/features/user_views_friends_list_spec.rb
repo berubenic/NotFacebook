cee13c6ddb98320edac3addcd1f409ec7fe010f2
Nicholas <berubenic@gmail.com>
2021-10-21 20:36:30 -0400

    remove bad specs

--- spec/features/user_views_friends_list_spec.rb
+++ spec/features/user_views_friends_list_spec.rb
@@ -18,46 +18,4 @@ RSpec.feature 'User views friends list' do
       expect(page).to have_content('friend one').once
     end
   end
-
-  context 'they have no friend' do
-    scenario 'they add a friend' do
-      user = create(:user)
-      create(:user, first_name: 'friend', last_name: 'one')
-
-      visit root_path
-
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
-
-      find('input[type="submit"]').click
-
-      click_on 'Friends'
-
-      expect(User.count).to eq 2
-
-      click_on 'Add Friend'
-
-      expect(user.pending_friend_requests_sent.count).to eq 1
-    end
-  end
-
-  context ' they have no friend' do
-    scenario 'they recieve a friend request' do
-      user = create(:user)
-      friend = create(:user, first_name: 'friend', last_name: 'one')
-
-      create(:friendship, user: friend, friend: user)
-
-      visit root_path
-
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
-
-      find('input[type="submit"]').click
-
-      click_on 'Friends'
-
-      expect(user.pending_friend_requests_recieved.count).to eq 1
-    end
-  end
 end
