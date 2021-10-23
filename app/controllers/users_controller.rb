736f78806d978b32f9ec4ab2940dc547db5b8824
Nicholas <berubenic@gmail.com>
2021-10-23 11:51:02 -0400

    Refactor: remove logic from user index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,16 @@ class UsersController < ApplicationController
   def index
     user = current_user
     @friends = user.friends
+    friends_ids = @friends.pluck(:id)
+
     @friends_sent = user.pending_friend_requests_sent
+    friends_sent_ids = @friends_sent.pluck(:id)
+
     @friends_recieved = user.pending_friend_requests_recieved
-    @strangers = User.all
+    friends_recieved_ids = @friends_recieved.pluck(:id)
+
+    ids = friends_ids + friends_sent_ids + friends_recieved_ids + [current_user.id]
+    @strangers = User.where.not(id: ids)
   end
 
   def delete_profile_image
