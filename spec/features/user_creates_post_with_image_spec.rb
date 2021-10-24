13c26a1674a696056422a40329533a42dd9355d6
Nicholas <berubenic@gmail.com>
2021-10-24 09:12:00 -0400

    FEATURE: user can remove picture when creating a new post

--- spec/features/user_creates_post_with_image_spec.rb
+++ spec/features/user_creates_post_with_image_spec.rb
@@ -38,4 +38,24 @@ RSpec.feature 'User creates post with image' do
       expect(page).to have_content 'Post image is too big'
     end
   end
+
+  context 'they remove a picture' do
+    scenario 'they see the page with the form' do
+      user = create(:user)
+
+      visit root_path
+
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+
+      find('input[type="submit"]').click
+
+      fill_in 'post_body', with: 'My first image'
+      attach_file('Add Image', Rails.root + 'spec/fixtures/green.png')
+      click_on 'Remove Image'
+
+      click_on 'Post'
+      expect(page).to_not have_css("img[src*='green.png']")
+    end
+  end
 end
