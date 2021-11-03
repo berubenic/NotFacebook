dcd31669d4dd27f46915c1f2dbc962abf54a5b3a
Nicholas <berubenic@gmail.com>
2021-11-02 20:56:17 -0400

    Facebook api fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_135531) do
+ActiveRecord::Schema.define(version: 2021_11_03_002611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2021_10_24_135531) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "deletion_requests", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "pid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pid"], name: "index_deletion_requests_on_pid"
   end
 
   create_table "friendships", force: :cascade do |t|
